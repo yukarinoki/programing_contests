b7790c5df88dc0d624be14f5c0bd88507622c5eb
yukarinoki <blkheartp@gmail.com>
2019-02-02 05:56:03 +0900

    modify topological sort (O(|V| + |E|)) "Testcases of AOJ are too weak."

--- joi2008-spring/topological_sort.cpp
+++ joi2008-spring/topological_sort.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 using namespace std;
 
 /******************************
     verified by http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B
 ******************************/
-
-int topological_sort(vector<int> &in_degree,vector<vector<int> > &dag, vector<int> &sorted){
+vector<int> sorted;
+/* int topological_sort(vector<int> &in_degree,vector<vector<int> > &dag){
     int n = dag.size();
     if(sorted.size() == n) return 0;
     for(int i=0;i<n;i++){
@@ -18,20 +19,47 @@ int topological_sort(vector<int> &in_degree,vector<vector<int> > &dag, vector<in
         }
     }
     return 0;
+} */
+
+
+
+int topological_sort(vector<vector<int> > &dag){
+    int n = dag.size();
+    vector<int> in_degree(n);
+    for(int i=0;i<n;i++){
+        for(auto x: dag[i]){
+            in_degree[x]++;
+        }
+    }
+    queue<int> node_set;
+    for(int i=0;i<n;i++){
+        if(in_degree[i] == 0){
+            node_set.push(i);
+        }
+    }
+    while(!node_set.empty()){
+        int node = node_set.front();
+        node_set.pop();
+        sorted.push_back(node);
+        for(auto x: dag[node]){
+            in_degree[x]--;
+            if(in_degree[x] == 0) node_set.push(x);
+        }
+    }
+    return 0;
 }
 int main(){
     int n,e; cin >> n >> e;
-    vector<int> in_degree(n);
-    for(int i=0;i<n;i++) in_degree[i] = 0;
+    //vector<int> in_degree(n);
+    //for(int i=0;i<n;i++) in_degree[i] = 0;
     vector<vector<int> > dag(n);
-    vector<int> sorted;
     for(int i=0;i<e;i++){
         int f,t; cin >> f >> t;
         dag[f].push_back(t);
-        in_degree[t]++;
+        //in_degree[t]++;
     }
  
-    for(int i=0;i<n;i++) topological_sort(in_degree, dag, sorted);
+    topological_sort(dag);
     for(int i=0;i<n;i++) cout << sorted[i] << endl;
     return 0;
 }
